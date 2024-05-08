c452be105dc53054fd8d2e7d266f89470ddff005
RodrigoGR08 <rodrigogarcia.ramalho@gmail.com>
2024-05-08 08:54:28 -0400

    Program to calculate area of a circle

---
+++ circleArea.py
@@ -0,0 +1,9 @@
+#The formula to calculate circle area is Area = pi * radius²
+pi = 3.14
+radius = float(input("Enter the radius: "))
+#To elevate numbers we use double asterisk(**) on python, where the number at right is the exponent
+
+area = pi * (radius**2)
+#"um" is Unit of Measurement
+print(f'The area of a circle with Radius: {radius}, is Area: {area}um²')
+
