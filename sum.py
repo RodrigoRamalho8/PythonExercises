90d9bd3c9fd1768b150367328888ca1dec4b2113
RodrigoGR08 <rodrigogarcia.ramalho@gmail.com>
2024-05-07 21:42:00 -0400

    Our first operation

---
+++ sum.py
@@ -0,0 +1,5 @@
+#Using int and input methods let's do a addition of two variables
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print(num1 + num2)
