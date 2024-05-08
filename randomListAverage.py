26e4b13fa2d8eb49f1b42112ce6fdb1d7180b5f9
RodrigoGR08 <rodrigogarcia.ramalho@gmail.com>
2024-05-08 13:20:24 -0400

    Calculating avarage of a random list

---
+++ randomListAverage.py
@@ -0,0 +1,13 @@
+import random
+
+listlen = int(input("Enter the length of our list: "))
+randomList = []
+
+for i in range(1,listlen+1):
+	i = random.randint(1,100)
+	randomList.append(i)
+	
+sumList = sum(randomList)
+result = sumList/len(randomList)
+
+print(f'Our list {randomList} have {len(randomList)} element/s resulting in: {result}')
