8bbfaa9764d5e5f2ad712a3b01042436252e4f4f
RodrigoGR08 <rodrigogarcia.ramalho@gmail.com>
2024-05-07 21:36:55 -0400

    Using int() method

---
+++ intMethod.py
@@ -0,0 +1,12 @@
+val = input("Enter a number: ")
+print(type(val))
+#It returns <class 'str'>
+val = int(val)
+print(type(val))
+#It returns <class 'int>, turning a String to a integer
+
+#We can do it as soon as we use input
+val2 = int(input("Enter another number: "))
+print(type(val2))
+
+
