1f9ff0038bd97d8cca9dd7baf42a0416b69c012f
Rodrigo Ramalho <rodrigo51685776@aluno.ms.senac.br>
2024-05-13 09:23:42 -0400

    Adding colours

--- schoolSystem/main.py
+++ schoolSystem/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 from tkcalendar import Calendar, DateEntry
 from classStudent import Student
@@ -6,16 +7,17 @@ from classStudent import Student
 root = Tk()
 root.geometry("500x500")
 root.title("Register window")
+root.configure(background='gray')
 
-fullName = Label(root,text="Full name: ")
+fullName = Label(root,text="Full name: ", font = ("Copperplate Gothic Light", 10), background="gray")
 fullName.place(relx = 0.4, rely=0.4,anchor=CENTER)
 fullNameEntrada = Entry(root)
-fullNameEntrada.place(relx = 0.6, rely=0.4,anchor=CENTER)
+fullNameEntrada.place(relx = 0.61, rely=0.4,anchor=CENTER)
 
-calendar = Label(root, text="Birth date")
+calendar = Label(root, text="Birth date",font = ("Copperplate Gothic Light", 10), background="gray")
 calendar.place(relx = 0.4, rely=0.45,anchor=CENTER)
-birthDateEntry = DateEntry(root, width=12,background='darkblue', foreground='white', borderwidth=2)
-birthDateEntry.place(relx = 0.58, rely = 0.45, anchor=CENTER)
+birthDateEntry = DateEntry(root, width=12,background='darkblue', foreground='white', borderwidth=2,font = ("Copperplate Gothic Light", 10) )
+birthDateEntry.place(relx = 0.61, rely = 0.45, width="126",anchor=CENTER)
 
 
 def onClickGetMatricula():
@@ -31,13 +33,12 @@ def onClickCadastro():
         messagebox.showerror("Error", f'Unable to register')
     
     
-    
-    
-botaoSair = Button(root, text="Exit", command=root.destroy)
-botaoSair.place(relx = 0.44, rely = 0.6, anchor=CENTER)
-botaoEnviar = Button(root, text="Register", command=onClickCadastro)
-botaoEnviar.place(relx = 0.54, rely = 0.6, anchor=CENTER)
-botaoMatricula = Button(root, text="List students", command = onClickGetMatricula)
-botaoMatricula.place(relx = 0.49, rely = 0.7, anchor=CENTER)
+  
+botaoSair = Button(root, text="Exit",font = ("Copperplate Gothic Light", 10),compound=tk.LEFT,command=root.destroy)
+botaoSair.place(relx = 0.35, rely = 0.6, anchor=CENTER)
+botaoMatricula = Button(root, text="List students",font = ("Copperplate Gothic Light", 10), command = onClickGetMatricula)
+botaoMatricula.place(relx = 0.5, rely = 0.6, anchor=CENTER)
+botaoEnviar = Button(root, text="Register",font = ("Copperplate Gothic Light", 10), command=onClickCadastro)
+botaoEnviar.place(relx = 0.685, rely = 0.6, anchor=CENTER)
 
 root.mainloop()
